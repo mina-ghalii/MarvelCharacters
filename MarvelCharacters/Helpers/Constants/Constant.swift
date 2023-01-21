ebd24047d8e14361093156d4f43ed81ca268f28a
Mina Atef <minaateffcis2015@gmail.com>
2023-01-21 23:34:00 +0200

    update: Constants

--- MarvelCharacters/Helpers/Constants/Constant.swift
+++ MarvelCharacters/Helpers/Constants/Constant.swift
@@ -17,7 +17,7 @@ struct CustomError {
     static let faildtoPasrse = ErrorDetails(key: "faild_to_parse", code: 0)
     static let noHttpResponse = ErrorDetails(key: "no_http_response", code: 1)
     static let noURL = ErrorDetails(key: "no_url", code: 2)
-    static let fileNotFound = ErrorDetails(key: "file_not_Found", code: 2)
+    static let fileNotFound = ErrorDetails(key: "file_not_Found", code: 3)
 }
 struct ErrorDetails {
     let key: String
